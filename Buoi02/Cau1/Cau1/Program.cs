caa277ed3ed09781aa27be42586eecd9a73dd5da
NPXuan <xuanphucit7575@gmail.com>
2024-11-01 19:03:56 +0700

    BTVN_Buoi02

---
+++ Buoi02/Cau1/Cau1/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace Cau1
+{
+    internal class Program
+    {
+
+        static double phanA()
+        {
+            double t = 2.125; // thoi gian
+            double A = 9; // bien do
+            double w = 5 * Math.PI;
+            double x0 =A * Math.Cos(Math.PI / 2);
+            double T = (2 * Math.PI) / w;
+
+            double s = (int)(4 * t / T) * A;
+            double s1 = A * Math.Cos(5 * Math.PI * 0.025 );
+
+            return s + (9 - s1);
+        }
+        static double phanB(double a, double b, double t)
+        {
+            double x0 = a * Math.Cos(Math.PI / 2);
+            double T = 2 / b;
+            double dt = (int)(4 * t / T);
+  
+            double d = dt * a;
+            double x01 = 0;
+            if(dt*T/4 != t)
+            {
+                x01 = a * Math.Cos(b * Math.PI * t + Math.PI / 2);
+                double o = b * Math.PI * (t - dt * T/4) ;
+
+                if((o > 0 && o < Math.PI / 2) || (o > Math.PI && o < 3 * Math.PI / 2))
+                    d = d + (a + x01);
+                else
+                    d = d + x01;
+            }
+
+            return d;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Quang duong vat di duoc la :" + phanA());
+            double a = 9;
+            double b = 2;
+            double t = 2;
+            Console.WriteLine("Quang duong vat A di duoc sau t la : " + phanB(9,5,2.125));
+        }
+    }
+}
---
+++ Buoi02/Cau2/Cau2/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace Cau2
+{
+    internal class Program
+    {
+        static double v0 = 20;
+        static double g = 9.8;
+        static double radian = 30 * Math.PI / 180;
+        static void phanA()
+        {
+            double v0x = v0 * Math.Cos(radian);
+            double v0y = v0 * Math.Sin(radian);
+
+            Console.WriteLine("Thanh phan van toc theo phuong ngang: " + v0x);
+            Console.WriteLine("Thanh phan van toc theo phuong doc: " + v0y);
+        }
+        static void phanB()
+        {
+            double v0y = v0 * Math.Sin(radian);
+            double tmax = v0y / g;
+
+            Console.WriteLine("Thoi gian len den diem cao nhat : " + tmax);
+        }
+        static void phanC()
+        {
+            double v0y = v0 * Math.Sin(radian);
+            double tmax = v0y / g;
+            double h = v0y * tmax - (g * tmax * tmax) / 2;
+
+            Console.WriteLine("Chieu cao cuc dai : " + h);
+        }
+        static void phanD()
+        {
+            double v0x = v0 * Math.Cos(radian);
+            double v0y = v0 * Math.Sin(radian);
+            double ttol = 2 * (v0y / g);
+            double R = v0x * ttol;
+
+            Console.WriteLine("Quang duong ngang vat di duoc : " + R);
+        }
+        static void Main(string[] args)
+        {
+            phanA();
+            phanB();
+            phanC();
+            phanD();
+        }
+    }
+}
